bd5822388ad83b5cd3cb3701f2a928d427ad3cf0
Yogesh Chaudhari <157873734+Yogesh7107@users.noreply.github.com>
2024-12-26 11:08:50 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
   content: [
     "./index.html",
@@ -6,18 +8,17 @@ export default {
   ],
   theme: {
     extend: {
-      boxShadow:{
+      boxShadow: {
         insideChatBox: "inset 0px 0px 3px 2px #c5cbd3",
         keyNumber: "0px 0px 15px 3px #c5cbd3",
         imgDisp: "0px 0px 25px 3px #a7a7a7",
       },
-      background:{
+      background: {
         slop: "linear-gradient(0deg, rgba(244,244,244,1) 50%, rgba(255,255,255,0.3781162123052346) 100%)"
       }
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 }
-
